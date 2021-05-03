ade545773faba4395f78a119e8d0924fc24f3529
Coralinav <mcna97@gmail.com>
2021-05-02 22:22:01 -0600

    5th validate text of El Salvador and Nicaragua, clicking in Asia and checking if Bacolob city exists

--- AutomationTest/focusTestMnavarro.js
+++ AutomationTest/focusTestMnavarro.js
@@ -23,11 +23,11 @@ describe("Iframes Lesson", () => {
 
   it("Step 3: Scrolling into 'Now Hiring' button and checking if it exists then clicking in 'locations' tab ", () => {
     const nowHiringButton=$("//a[@href='https://www.focusservices.com/apply/']");
-    nowHiringButton.scrollIntoView();
+    nowHiringButton.scrollIntoView({block: "center", behavior: "smooth"});
     browser.pause(3000);
     nowHiringButton.isExisting();
     const locationButton=$("//a[@href='https://www.focusservices.com/locations/']");
-    locationButton.scrollIntoView();
+    locationButton.scrollIntoView({block: "center", behavior: "smooth"});
     browser.pause(3000);
     locationButton.click()
 
@@ -36,11 +36,27 @@ describe("Iframes Lesson", () => {
   it("Step 4: Looking for North America link and doing an assertion ", () => {
     const northAmericaLink=$('//span[text()="North America"]');
     expect(northAmericaLink).toBeVisible();
+    northAmericaLink.scrollIntoView({block: "center", behavior: "smooth"});
     browser.pause(3000);
     const centralAmericaLink=$('//span[text()="Central America"]');
     browser.pause(3000);
-    centralAmericaLink.scrollIntoView();
+    centralAmericaLink.scrollIntoView({block: "center", behavior: "smooth"});
     centralAmericaLink.click()
+  });
+
+  it("Steps 5: Validate if El Salvador & Nicaragua Exists and clicking in Asia and verifying a text", () => {
+    const elSalvadorandNicaragua=$('(//div[@class="avia_textblock  "])[13]/h2');
+    elSalvadorandNicaragua.isDisplayed();
+    browser.pause(3000);
+    const AsiaLink=$('//span[text()="Asia"]');
+    browser.pause(3000);
+    AsiaLink.scrollIntoView({block: "center", behavior: "smooth"});
+    AsiaLink.click()
+    browser.pause(3000);
+    const bacolobCity=$('(//div[@class="avia_textblock  "]/h3)[12]');
+    bacolobCity.isDisplayed();
+    bacolobCity.saveScreenshot('bacolobCity,PhilippinesExist.png'); 
+
   });
 
 });
